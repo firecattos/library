60ac2dd1a8c459c3da6947af4b399a8ddf6309df
Gabriele <gabrimassi@rocketmail.com>
2024-07-15 23:37:15 +0200

    Working functions, code needs a cleaning and organization.

--- library.js
+++ library.js
@@ -1,7 +1,7 @@
 const myLibrary=[];
 
 
-function Book(title, author, pages, read){ //add genre maybe
+function Book(title, author, pages, read){ //add genre and date of publication maybe
     this.title = title;
     this.author = author;    
     this.pages = pages;
@@ -19,7 +19,11 @@ https://www.freecodecamp.org/news/how-to-submit-a-form-with-javascript/
 https://www.freecodecamp.org/news/how-to-build-a-modal-with-javascript/
 */
 
-Book.prototype.removeBook = function(){}
+Book.prototype.removeBook = function(index){
+    console.log("Test removeBook called from book "+index);
+    myLibrary.splice(index, 1);
+    bookDisplay(myLibrary);
+}
 
 Book.prototype.displayBook = function(){
     console.log("test display, book "+this.position);
@@ -32,13 +36,17 @@ const newBook=document.getElementById("newBook");
 });*/
 newBook.addEventListener("submit", createBook);
 
+//is <dialog> worth? check
+
 function createBook(e){
     e.preventDefault();
     console.log("Prevented Default 1");
     let newBookTitle=document.getElementById("newBookTitle").value;
     let newBookAuthor=document.getElementById("newBookAuthor").value;
     let newBookPages=document.getElementById("newBookPages").value;
-    let newBookRead=document.getElementById("newBookRead").checked;
+    //let newBookRead=document.getElementById("newBookRead").checked;
+    let newBookRead;
+    if(document.querySelector("#newBookRead").value==="Read") newBookRead=true;
     console.log("book: "+newBookTitle+", "+newBookAuthor+", "+newBookPages+", "+newBookRead);
     let createBook=new Book(newBookTitle, newBookAuthor, newBookPages, newBookRead);
     createBook.addBook();
@@ -46,12 +54,13 @@ function createBook(e){
     bookDisplay(myLibrary);
 
     inputModal.style.display="none";
+    newBook.reset();
 }
 
 //Test books
-let book1=new Book("book1", "testing", "111", true);
+let book1=new Book("The art of war", "Sun Tzu", "1160", true);
 book1.addBook();
-let book2=new Book("book2", "testing", "222");
+let book2=new Book("The C++ programming language", "Bjarne Stroustrup", "1370");
 book2.addBook();
 let book3=new Book("book3", "testing", "333", true);
 book3.addBook();
@@ -68,22 +77,32 @@ function bookDisplay(myLibrary){
 
     //while(bookshelf.firstChild) bookshelf.removeChild(firstChild);
 
-    myLibrary.forEach((book)=>{
+    myLibrary.forEach((book, index)=>{
         const bookCard=document.createElement("div");
         bookCard.className="book";
 
-        const title=document.createElement("h3");
-        title.textContent=book.title;
+        const title=document.createElement("p");
+        title.textContent="\""+book.title+"\"";
         bookCard.appendChild(title);
         const author=document.createElement("p");
         author.textContent=book.author;
         bookCard.appendChild(author);
         const pages=document.createElement("p");
-        pages.textContent=book.pages;
+        pages.textContent=book.pages+" pages";
         bookCard.appendChild(pages);
-        const read=document.createElement("p");
+        const read=document.createElement("button");
+        //read.id="readStatus";
         read.textContent=book.read ? "Read" : "Not read";
+        //read.addEventListener("click", changeStatus);
+        read.onclick=changeStatus;
         bookCard.appendChild(read);
+        
+        const removeButton=document.createElement("button");
+        removeButton.textContent="Remove";
+        removeButton.onclick= ()=>{
+            book.removeBook(index);
+        };
+        bookCard.appendChild(removeButton);
         //after everything:
         bookshelf.appendChild(bookCard);
     })
@@ -91,17 +110,30 @@ function bookDisplay(myLibrary){
 
 bookDisplay(myLibrary); //Display test books
 
-const inputModal=document.getElementById("inputModal");
+const inputModal=document.getElementById("inputModal"); //reused
 //When clicked, this button makes an overlay with the modal appear
 const addNewBook=document.getElementById("createBook");
 addNewBook.addEventListener("click", ()=>{
     inputModal.style.display="block";
+    //inputModal.showModal();
+});
+/*  <DIALOG> TESTING*/
+const readChecker=document.getElementById("newBookRead"); //Read button in form functionality
+readChecker.addEventListener("click", ()=>{
+//readChecker.onclick=changeStatus;
+    if(readChecker.value==="Not read") readChecker.value="Read";
+    else if(readChecker.value==="Read") readChecker.value="Not read";
 });
 
 //To exit the modal, just click anywhere outside of its elements
 window.onclick=(e)=>{
     if(e.target==inputModal) inputModal.style.display="none";
-}
+}/**///commented for testing with <dialog>
+
+function changeStatus(e){ //Changes the read status displayed in the book card
+    if(e.target.innerText==="Not read") e.target.innerText="Read";
+    else if(e.target.innerText==="Read") e.target.innerText="Not read";
+};
 
 //listBtn function
 /*const listBooksBtn=document.getElementById("listBtn");
